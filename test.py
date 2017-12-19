f605be1a4d0de962d49e1a79c22d0eb6471491cd
OldPoe <34619858+OldPoe@users.noreply.github.com>
2017-12-19 09:05:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-from math import sqrt  
-def isprime(n):  
-    k = int(sqrt(n))  
-    for i in range(2,k+1):  
-        if n % i == 0:  
-            return 0  
-    return 1  
-      
-if __name__ == '__main__':  
-    for n in range(101, 201):  
-        if isprime(n) ==1:  
-            print(n, end = ' ')  
+
